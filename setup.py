d20f528ce4f7d8652898d1ee04acd902293623c6
JGabrielJ <gjacinto0707@gmail.com>
2022-02-02 13:36:27 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import cx_Freeze
+
+
+executables = [cx_Freeze.Executable('main.py')]
+
+cx_Freeze.setup(
+    name = 'ProjetoFFP',
+    options = {'build_exe': {'packages': ['pygame', 'PySimpleGUI', 'emoji'],
+                             'include_files': ['icon.ico', 'song.mp3']}},
+
+    executables = executables
+    
+)
